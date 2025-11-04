81b5393ca3a0bc699261ee4e2dffa4e08b0be1d2
leo <leoyng95@gmail.com>
2025-11-05 00:52:39 +0100

    creation of the component allowing the selection of the Pokémon

--- front-end/pokebet/src/App.js
+++ front-end/pokebet/src/App.js
@@ -1,14 +1,20 @@
-import './App.css';
-import bg_logo from './assets/BG_Logo.svg';
-import logo from './assets/PokeBet_Group.png';
+import "./App.css"
+import bg_logo from "./assets/BG_Logo.svg"
+import logo from "./assets/PokeBet_Group.png"
+import PokemonSelector from "../src/component/pokemonSelector/PokemonSelector.jsx"
 
 function App() {
-  return (
-    <div className="App">
-      <img src={bg_logo} alt='PokeBet_BG'></img>
-      <img className="logo" src={logo} alt='PokeBet_Logo'></img>
-    </div>
-  );
+	const handleSelectClick = () => {
+		console.log("Clic !")
+	}
+	return (
+		<div className='App'>
+			<img src={bg_logo} alt='PokeBet_BG'></img>
+			<img className='logo' src={logo} alt='PokeBet_Logo'></img>
+
+			<PokemonSelector onClick={handleSelectClick} />
+		</div>
+	)
 }
 
-export default App;
+export default App
--- front-end/pokebet/src/component/pokemonSelector/PokemonSelector.jsx
+++ front-end/pokebet/src/component/pokemonSelector/PokemonSelector.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 import './PokemonSelector.css';
 
-import Pokeball from '../../../assets/Pokeball.png';
+import Pokeball from '../../assets/Pokeball.png';
 
 function PokemonSelector({ onClick, text = "Sélectionnez votre Pokémon" }) {
   
   return (
-    <button className="pokemon-selector-button" onClick={onClick}>
-      <img src={Pokeball} alt="Pokeball" className="pokemon-selector-icon" />
-      <span>{text}</span>
-    </button>
+    <div className="pokemon-selector-container">
+      <button className="pokeball-btn" onClick={onClick}>
+        <img src={Pokeball} alt="Pokeball" className="pokeball-img" />
+      </button>
+      <span className="select-text">{text}</span>
+    </div>
   );
 }
 
