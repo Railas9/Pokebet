d2d8afb2c76809d2d2296bb0ee7f031105e02e6a
Sacha <sacha.harel@gmail.com>
2025-11-04 11:23:44 +0100

    UPDATES // Back-End init

--- front-end/pokebet/src/App.js
+++ front-end/pokebet/src/App.js
@@ -1,12 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
-      </header>
+      <img src='..\assets\PokeBet_Group.png' alt='PokeBet_Logo'></img>
     </div>
   );
 }
