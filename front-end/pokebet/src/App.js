601f18bd688a83ac7e5442fd9bb48250b2234c62
leo <leoyng95@gmail.com>
2025-11-05 16:12:36 +0100

    update

--- front-end/pokebet/src/App.js
+++ front-end/pokebet/src/App.js
@@ -2,18 +2,27 @@ import "./App.css"
 import bg_logo from "./assets/BG_Logo.svg"
 import logo from "./assets/PokeBet_Group.svg"
 import PokemonSelector from "../src/component/pokemonSelector/PokemonSelector.jsx"
+import React, { useState } from "react";
 
 function App() {
-	const handleSelectClick = () => {
-		console.log("Clic !")
-	}
+	const [selectedLeft, setSelectedLeft] = useState(null);
+	const [selectedRight, setSelectedRight] = useState(null);
+
 	return (
 		<div className='App'>
 			<div className='header'>
 				<img className='bg-logo' src={bg_logo} alt='PokeBet_BG'></img>
 				<img className='logo' src={logo} alt='PokeBet_Logo'></img>
+			</div>
 
-				<PokemonSelector onClick={handleSelectClick} />
+			<div className='arena'>
+				<div className='side left'>
+					<PokemonSelector text={selectedLeft || "Sélectionnez votre Pokémon"} onSelect={setSelectedLeft} />
+				</div>
+				<div className='divider' />
+				<div className='side right'>
+					<PokemonSelector text={selectedRight || "Sélectionnez votre Pokémon"} onSelect={setSelectedRight} />
+				</div>
 			</div>
 		</div>
 	)
--- front-end/pokebet/src/component/pokemonSelector/PokemonSelector.jsx
+++ front-end/pokebet/src/component/pokemonSelector/PokemonSelector.jsx
@@ -1,16 +1,49 @@
-import React from "react";
+import React, { useState, useRef, useEffect } from "react";
 import './PokemonSelector.css';
-
 import Pokeball from "../../assets/PokeBall.svg";
+import PokeballSelected from "../../assets/PokeBall-1.svg";
+import pokemonList from "./pokemonList";
 
-function PokemonSelector({ onClick, text = "Sélectionnez votre Pokémon" }) {
-  
+function PokemonSelector({ onSelect, text = "Sélectionnez votre Pokémon" }) {
+  const [open, setOpen] = useState(false);
+  const [selected, setSelected] = useState(null);
+  const ref = useRef();
+
+  useEffect(() => {
+    function handleClickOutside(event) {
+      if (ref.current && !ref.current.contains(event.target)) {
+        setOpen(false);
+      }
+    }
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => document.removeEventListener("mousedown", handleClickOutside);
+  }, []);
+
+  const handleSelect = (pokemon) => {
+    setSelected(pokemon);
+    setOpen(false);
+    if (onSelect) onSelect(pokemon);
+  };
+
   return (
-    <div className="pokemon-selector-container">
-      <button className="pokeball-btn" onClick={onClick}>
-        <img src={Pokeball} alt="Pokeball" className="pokeball-svg" />
+    <div className="pokemon-selector-container" ref={ref}>
+      <button className="pokeball-btn" onClick={() => setOpen((o) => !o)}>
+        <img src={selected ? PokeballSelected : Pokeball} alt="Pokeball" className="pokeball-svg" />
       </button>
-      <span className="select-text">{text}</span>
+      <span className="select-text">{selected || text}</span>
+      {open && (
+        <ul className="dropdown-list">
+          {pokemonList.map((pokemon) => (
+            <li
+              key={pokemon}
+              className="dropdown-item"
+              onClick={() => handleSelect(pokemon)}
+            >
+              {pokemon}
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
